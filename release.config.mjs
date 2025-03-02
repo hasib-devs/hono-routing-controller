447f54020e9d9c2d824d522da3b0204ae5c61c5e
Hasibur Rahman <hasib.webdev@gmail.com>
2025-03-02 10:57:23 +0600

    chore: add semantic-release plugins for changelog and git integration

--- release.config.mjs
+++ release.config.mjs
@@ -2,15 +2,23 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-    branches: ["main"],
+    branches: ["main", "next", { name: "beta", prerelease: true }, { name: "alpha", prerelease: true }],
     plugins: [
         "@semantic-release/release-notes-generator",
         "@semantic-release/commit-analyzer",
-        "@semantic-release/changelog",
         "@semantic-release/github",
         "@semantic-release/npm",
+        [
+            "@semantic-release/changelog",
+            {
+              "changelogFile": "docs/CHANGELOG.md"
+            }
+          ],
+          [
+            "@semantic-release/git",
+            {
+              "assets": ["docs/CHANGELOG.md"]
+            }
+          ],
     ],
-    ci: false,
-    dryRun: true,
-    debug: true,
   };
