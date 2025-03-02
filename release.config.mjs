51597582a8cc7f8a149fc62edad772fe00b64562
Hasibur Rahman <hasib.webdev@gmail.com>
2025-03-02 11:31:49 +0600

    chore: update release configuration to simplify branch management and ensure changelog generation

--- release.config.mjs
+++ release.config.mjs
@@ -2,23 +2,26 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-    branches: ["main", "next", { name: "beta", prerelease: true }, { name: "alpha", prerelease: true }],
-    plugins: [
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/github",
-        "@semantic-release/npm",
-        [
-            "@semantic-release/changelog",
-            {
-              "changelogFile": "docs/CHANGELOG.md"
-            }
-          ],
-          [
-            "@semantic-release/git",
-            {
-              "assets": ["docs/CHANGELOG.md"]
-            }
-          ],
+  branches: [
+    { name: "main" },
+    { name: "next", prerelease: true },
+  ],
+  plugins: [
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "docs/CHANGELOG.md"
+      }
     ],
-  };
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["docs/CHANGELOG.md"]
+      }
+    ],
+  ],
+};
