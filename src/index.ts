5068e86612bc865cb43348bea19d4e4930d15779
Hasibur Rahman <hasib.webdev@gmail.com>
2025-03-08 07:03:37 +0600

    chore: remove createServer function; streamline server initialization in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
+import { Server } from './core/Server';
+import type { ServerOptions } from './types';
 
 // -------------------------------------------------------------------------
 // Main exports
-
-
 // -------------------------------------------------------------------------
 
-export * from './di/Container';
-export * from './core/createServer';
 export * from './decorators/Controller';
 export * from './decorators/HttpMethods';
 export * from './decorators/Inject';
+export * from './di/Container';
+
+export function createServer(app: any, options: ServerOptions): void {
+    Server.create(app, options);
+}
